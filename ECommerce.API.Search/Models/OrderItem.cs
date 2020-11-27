9ba6d3d5245de245226f0678d269953809fcafba
Dmytro Sudoma <Dmytro_Sudoma@epam.com>
2020-11-27 14:35:19 +0200

    Add microservices support

--- ECommerce.API.Search/Models/OrderItem.cs
+++ ECommerce.API.Search/Models/OrderItem.cs
@@ -11,7 +11,6 @@ namespace ECommerce.API.Search.Models
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public string CustomerName { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
     }
